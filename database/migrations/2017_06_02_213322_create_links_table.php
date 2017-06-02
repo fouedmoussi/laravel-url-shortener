7082d4a203b6d2542c7f8b7ed13a4f1fec60cf41
FouedMoussi <MoussiFoued@outlook.fr>
2017-06-02 22:55:09 +0100

    setup laravelcollective package

---
+++ database/migrations/2017_06_02_213322_create_links_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateLinksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('links', function(Blueprint $table) {
+                $table->increments('id');
+                $table->text('url');
+                $table->string('hash',400);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('links');
+    }
+}
